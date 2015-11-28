ae09c0419409c00614f9ee7b924dcbc12d1f9fb3
Elisa Kazan <elisa.kazan@gmail.com>
2015-11-28 17:37:29 -0500

    Jack won the argument, changed variable names

--- defs.h
+++ defs.h
@@ -1,16 +1,18 @@
 #define MAX_STR 32
 #define NUM_LETTERS 26
+#define C_OK 1
+#define C_NOK 0
 
 /*  Structures  */
 typedef struct
 {
     enum gamestate state;
-    char p1_word[MAX_STR];
-    char p2_word[MAX_STR];
-    char p1_incorrect[NUM_LETTERS + 1];
-    char p2_incorrect[NUM_LETTERS + 1];
-    char p1_solution[MAX_STR];
-    char p2_solution[MAX_STR];
+    char us_word[MAX_STR];
+    char them_word[MAX_STR];
+    char us_incorrect[NUM_LETTERS + 1];
+    char them_incorrect[NUM_LETTERS + 1];
+    char us_solution[MAX_STR];
+    char them_solution[MAX_STR];
 } Game;
 
 /*  Functions  */
