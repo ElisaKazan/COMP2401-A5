9351d7a8c0a46ed1625730b169aeedf662673cbd
Jack McCracken <jackamc19@gmail.com>
2015-11-30 16:34:45 -0500

    Fix a silly mistake - didn't include a header!

--- network.c
+++ network.c
@@ -1,5 +1,6 @@
 #include "defs.h"
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -55,5 +56,4 @@ void activate_socket_server() {
 }
 
 void end_turn(Game *g, char *buffer) {
-
 }
